603d0905717a9074685f004f06c48be53accfef1
DevGoran <goran.vlacic@hotmail.com>
2021-08-04 18:50:38 +0200

    Updates

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.newsapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -10,14 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Adapter for the list of news
+     */
+    private NewsAdapter mAdapter;
 
     /**
      * Sample JSON response for a query
      * "https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test"
      */
-    private static final String JSON_RESPONSE = "{\"response\":{\"status\":\"ok\",\"userTier\":\"developer\",\"total\":18255,\"startIndex\":1,\"pageSize\":10,\"currentPage\":1,\"pages\":1826,\"orderBy\":\"relevance\",\"results\":[{\"id\":\"commentisfree/2021/jul/24/vaccine-passports-rights-freedoms-covid\",\"type\":\"article\",\"sectionId\":\"commentisfree\",\"sectionName\":\"Opinion\",\"webPublicationDate\":\"2021-07-24T11:00:21Z\",\"webTitle\":\"It's time for a serious debate about vaccine passports | Shami Chakrabarti\",\"webUrl\":\"https://www.theguardian.com/commentisfree/2021/jul/24/vaccine-passports-rights-freedoms-covid\",\"apiUrl\":\"https://content.guardianapis.com/commentisfree/2021/jul/24/vaccine-passports-rights-freedoms-covid\",\"isHosted\":false,\"pillarId\":\"pillar/opinion\",\"pillarName\":\"Opinion\"},{\"id\":\"politics/2021/jul/01/labour-mp-shares-his-struggles-with-addiction-in-commons-debate-for-pride\",\"type\":\"article\",\"sectionId\":\"world\",\"sectionName\":\"World news\",\"webPublicationDate\":\"2021-07-01T18:00:15Z\",\"webTitle\":\"Labour MP shares addiction struggles in Commons Pride debate\",\"webUrl\":\"https://www.theguardian.com/politics/2021/jul/01/labour-mp-shares-his-struggles-with-addiction-in-commons-debate-for-pride\",\"apiUrl\":\"https://content.guardianapis.com/politics/2021/jul/01/labour-mp-shares-his-struggles-with-addiction-in-commons-debate-for-pride\",\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"commentisfree/2021/may/22/the-free-speech-law-will-make-university-debate-harder-not-easier\",\"type\":\"article\",\"sectionId\":\"commentisfree\",\"sectionName\":\"Opinion\",\"webPublicationDate\":\"2021-05-22T09:00:56Z\",\"webTitle\":\"The 'free speech' law will make university debate harder, not easier | David Renton\",\"webUrl\":\"https://www.theguardian.com/commentisfree/2021/may/22/the-free-speech-law-will-make-university-debate-harder-not-easier\",\"apiUrl\":\"https://content.guardianapis.com/commentisfree/2021/may/22/the-free-speech-law-will-make-university-debate-harder-not-easier\",\"isHosted\":false,\"pillarId\":\"pillar/opinion\",\"pillarName\":\"Opinion\"},{\"id\":\"business/2021/may/21/rich-list-2021-results-will-renew-debate-on-wealth-tax-in-britain\",\"type\":\"article\",\"sectionId\":\"business\",\"sectionName\":\"Business\",\"webPublicationDate\":\"2021-05-21T16:05:10Z\",\"webTitle\":\"Rich List 2021: results will renew debate on wealth tax in Britain\",\"webUrl\":\"https://www.theguardian.com/business/2021/may/21/rich-list-2021-results-will-renew-debate-on-wealth-tax-in-britain\",\"apiUrl\":\"https://content.guardianapis.com/business/2021/may/21/rich-list-2021-results-will-renew-debate-on-wealth-tax-in-britain\",\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"politics/2021/mar/11/snp-focus-on-scottish-independence-is-wrong-debate-keir-starmer\",\"type\":\"article\",\"sectionId\":\"politics\",\"sectionName\":\"Politics\",\"webPublicationDate\":\"2021-03-11T17:06:03Z\",\"webTitle\":\"SNP focus on Scottish independence is 'wrong debate', says Starmer \",\"webUrl\":\"https://www.theguardian.com/politics/2021/mar/11/snp-focus-on-scottish-independence-is-wrong-debate-keir-starmer\",\"apiUrl\":\"https://content.guardianapis.com/politics/2021/mar/11/snp-focus-on-scottish-independence-is-wrong-debate-keir-starmer\",\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"music/2021/apr/10/music-streaming-debate-what-songwriter-artist-and-industry-insider-say-publication-parliamentary-report\",\"type\":\"article\",\"sectionId\":\"music\",\"sectionName\":\"Music\",\"webPublicationDate\":\"2021-04-10T07:00:01Z\",\"webTitle\":\"The music streaming debate: what the artists, songwriters and industry insiders say\",\"webUrl\":\"https://www.theguardian.com/music/2021/apr/10/music-streaming-debate-what-songwriter-artist-and-industry-insider-say-publication-parliamentary-report\",\"apiUrl\":\"https://content.guardianapis.com/music/2021/apr/10/music-streaming-debate-what-songwriter-artist-and-industry-insider-say-publication-parliamentary-report\",\"isHosted\":false,\"pillarId\":\"pillar/arts\",\"pillarName\":\"Arts\"},{\"id\":\"commentisfree/2021/jun/11/bbc-drama-britain-prisons-jimmy-mcgovern-time\",\"type\":\"article\",\"sectionId\":\"commentisfree\",\"sectionName\":\"Opinion\",\"webPublicationDate\":\"2021-06-11T14:03:23Z\",\"webTitle\":\"Will it take a BBC drama to finally start a rational debate about Britain\u2019s prisons? | Owen Jones\",\"webUrl\":\"https://www.theguardian.com/commentisfree/2021/jun/11/bbc-drama-britain-prisons-jimmy-mcgovern-time\",\"apiUrl\":\"https://content.guardianapis.com/commentisfree/2021/jun/11/bbc-drama-britain-prisons-jimmy-mcgovern-time\",\"isHosted\":false,\"pillarId\":\"pillar/opinion\",\"pillarName\":\"Opinion\"},{\"id\":\"world/2021/mar/10/mps-hit-back-after-india-summons-envoy-over-farmers-protest-debate\",\"type\":\"article\",\"sectionId\":\"world\",\"sectionName\":\"World news\",\"webPublicationDate\":\"2021-03-10T16:29:33Z\",\"webTitle\":\"MPs hit back after India summons envoy over farmers' protest debate\",\"webUrl\":\"https://www.theguardian.com/world/2021/mar/10/mps-hit-back-after-india-summons-envoy-over-farmers-protest-debate\",\"apiUrl\":\"https://content.guardianapis.com/world/2021/mar/10/mps-hit-back-after-india-summons-envoy-over-farmers-protest-debate\",\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"uk-news/2021/feb/21/keep-out-of-bristols-slaver-street-names-debate-ministers-are-told\",\"type\":\"article\",\"sectionId\":\"uk-news\",\"sectionName\":\"UK news\",\"webPublicationDate\":\"2021-02-21T08:15:55Z\",\"webTitle\":\"Keep out of Bristol\u2019s slaver street names debate, ministers are told\",\"webUrl\":\"https://www.theguardian.com/uk-news/2021/feb/21/keep-out-of-bristols-slaver-street-names-debate-ministers-are-told\",\"apiUrl\":\"https://content.guardianapis.com/uk-news/2021/feb/21/keep-out-of-bristols-slaver-street-names-debate-ministers-are-told\",\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"society/2021/jan/18/tories-oppose-universal-credit-cut-keir-starmer-labour\",\"type\":\"article\",\"sectionId\":\"society\",\"sectionName\":\"Society\",\"webPublicationDate\":\"2021-01-18T14:35:48Z\",\"webTitle\":\"Boris Johnson believes political debate should be 'kind and civil'\",\"webUrl\":\"https://www.theguardian.com/society/2021/jan/18/tories-oppose-universal-credit-cut-keir-starmer-labour\",\"apiUrl\":\"https://content.guardianapis.com/society/2021/jan/18/tories-oppose-universal-credit-cut-keir-starmer-labour\",\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"}]}}";
+    private static final String JSON_RESPONSE = "https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,26 +34,57 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the layout.
         setContentView(R.layout.activity_main);
 
-        // Get the list of articles from {@link QueryUtils}
-        ArrayList<News> articles = QueryUtils.extractFeatureFromJson(JSON_RESPONSE);
+        // Create an {@link AsyncTask} to perform the HTTP request to the given URL
+        // on a background thread. When the result is received on the main UI thread,
+        // then update the UI.
+        NewsAsyncTask task = new NewsAsyncTask();
+        task.execute(JSON_RESPONSE);
 
         // Find a reference to the {@link ListView} in the layout.
         ListView newsListView = findViewById(R.id.list);
 
-        // Create a new {@link ArrayAdapter} of articles.
-        NewsAdapter adapter = new NewsAdapter(
-                this, articles);
+        // Create a new adapter that takes an empty list of news as input
+        mAdapter = new NewsAdapter(this, new ArrayList<>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        newsListView.setAdapter(adapter);
+        newsListView.setAdapter(mAdapter);
 
-        // Opens the selected article on click.
-        newsListView.setOnItemClickListener((parent, view, position, id) -> {
-            String url = articles.get(position).getUrl();
-            Intent openArticle = new Intent(Intent.ACTION_VIEW);
-            openArticle.setData(Uri.parse(url));
-            startActivity(openArticle);
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected news.
+        newsListView.setOnItemClickListener((adapterView, view, position, l) -> {
+            // Find the current news that was clicked on
+            News currentENews = mAdapter.getItem(position);
+
+            // Convert the String URL into a URI object (to pass into the Intent constructor)
+            Uri newsUri = Uri.parse(currentENews.getUrl());
+
+            // Create a new intent to view the news URI
+            Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+            // Send the intent to launch a new activity
+            startActivity(websiteIntent);
         });
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class NewsAsyncTask extends AsyncTask<String, Void, List<News>> {
+
+        @Override
+        protected List<News> doInBackground(String... urls) {
+            return QueryUtils.fetchNewsData(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<News> data) {
+            // Clear the adapter of previous news data.
+            mAdapter.clear();
+
+            // If there is a valid list of {@link News}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+        }
     }
 }
